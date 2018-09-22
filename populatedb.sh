585496a4f0f1b0fe540655845f36fe298562d431
Zi Gao <zi.s.gao@gmail.com>
2018-09-21 21:58:23 -0400

    completed line item addition feature

--- populatedb.sh
+++ populatedb.sh
@@ -1,5 +1,15 @@
+# Creating shops
+
 ./run_curl_shopify.sh '{"shopname": "BestBuy", "description": "A tech shop that sells tech stuff"}' api/create_shop
 ./run_curl_shopify.sh '{"shopname": "Blockbuster", "description": "The largest chain DVD rental store ;)"}' api/create_shop
 ./run_curl_shopify.sh '{"shopname": "Louis Vuitton", "description": "An expensive store for people that want clout"}' api/create_shop
+
+
+# Populating shops
 ./run_curl_shopify.sh '{"shopname": "Louis Vuitton", "productname": "keychain", "price": 250}' api/add_product
 ./run_curl_shopify.sh '{"shopname": "Louis Vuitton", "productname": "leather wallet", "price": 1500}' api/add_product
+
+
+# Population shops line items and such
+./run_curl_shopify.sh '{"shopname": "BestBuy", "productname": "iPhoneX", "quantity": 2, "detail": "black"}' api/add_lineitem
+./run_curl_shopify.sh '{"shopname": "BestBuy", "productname": "iPhoneX", "quantity": 1, "detail": "black", "orderID": "28372"}' api/add_lineitem
--- repos/lineitem.js
+++ repos/lineitem.js
@@ -2,30 +2,52 @@ module.exports = {
     addToProduct: async function (db, shopname, query) {
         let curr = await db.collection('shops').findOne({name: shopname});
         if (curr != null){
-            for (let i = 0; i < curr.products.length; i++){
-                if (curr.products[i].name == query.productname){
-                    await db.collection('shops').updateOne({name: shopname}, {$push: {orders: {}}}, (err, res) => {
-                        if (err) {
-                            console.log(err);
-                            return err;
-                        }
-                        console.log(`Successfully added product to ${query.productname}`);
-                    });
-                }
+            let product = curr.products[query.productname];
+            if (product != null){
+                query['price'] = product.price;
+                product.lineitems[`${query.detail}`] = query;
+                await db.collection('shops').updateOne({name: shopname}, {$set: {products: curr.products}}, (err, res) => {
+                    if (err) {
+                        console.log(err);
+                        return err;
+                    }
+                    console.log(`Successfully added line item ${query.detail} to ${query.productname}`);
+                });
+
+                return `Successfully added line item ${query.detail} to ${query.productname} \n`;
+            } else {
+                return `The product ${query.productname} does not exist! \n`
             }
-            
+        } else {
+            return `Shop with ${shopname} does not exist! \n`;
         }
-        return `Successfully added lineitem to ${query.productname}`;
     },
 
-    addToProduct: async function (db, shopname, orderID) {
-        await db.collection('shops').updateOne({name: shopname}, {$pull: {orders: {id: orderID}}}, (err, res) => {
-            if (err) {
-                console.log(err);
-                return err;
+    addToOrder: async function (db, shopname, orderID, query) {
+        let curr = await db.collection('shops').findOne({name: shopname});
+        if (curr != null){
+            let order = curr.orders[orderID];
+            let product = curr.products[query.productname];
+            if (order != null && product.lineitems[`${query.detail}`] != null){
+                query['price'] = product.price;
+                order.lineitems[`${query.productname}, ${query.detail}`] = query;
+                order.totalprice = order.totalprice + product.price*query.quantity;
+                await db.collection('shops').updateOne({name: shopname}, {$set: {orders: curr.orders}}, (err, res) => {
+                    if (err) {
+                        console.log(err);
+                        return err;
+                    }
+                    console.log(`Successfully added line item ${query.productname}, ${query.detail} to order ${orderID}`);
+                });
+
+                return `Successfully added line item ${query.productname}, ${query.detail} to order ${orderID} \n`;
+            } else {
+                return `Please check to make sure that orderID is valid and product with specified line item detail exists \n`;
             }
-            console.log(`Successfully deleted ${orderID} from ${shopname}`);
-        });
-        return `Successfully deleted ${orderID} from ${shopname}`;
-    }
+        } else {
+            return `Shop with ${shopname} does not exist! \n`;
+        }
+    },
+
+
 }
